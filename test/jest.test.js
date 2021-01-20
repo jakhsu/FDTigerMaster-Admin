ff05b57ecfd4093d1689dcdf7d61e01d21c42da8
jackhsu-git <contactjackhsu@gmail.com>
2021-01-20 16:15:54 +0800

    add jest as testing util

---
+++ test/jest.test.js
@@ -0,0 +1,11 @@
+const add = require("./add");
+const expect = require("chai").expect;
+
+describe("測試add函數", () => {
+    it('5+5預期: 10', () => {
+        expect(add(5, 5)).to.be.equal(10);
+    }, 500);
+    it('測試5+15預期20', () => {
+        expect(add(5, 15)).to.be.equal(20);
+    }, 500);
+})
