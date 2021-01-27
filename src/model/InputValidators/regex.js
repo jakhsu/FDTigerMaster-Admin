4791591252f072a78462b1db026947818ee6a440
jackhsu-git <contactjackhsu@gmail.com>
2021-01-27 10:00:10 +0800

    refractor: remove unused code

--- src/model/InputValidators/regex.js
+++
@@ -1,8 +0,0 @@
-export default function (inputType) {
-    switch (inputType) {
-        case "skillId":
-            return new RegExp(/^TM-[A-Z]{1}[0-9]{4}00$/, 'i');
-        case "categoryId":
-            return new RegExp(/^TM-[A-Z]{1}[0-9]{4}.+(?<!00)$/, 'i');
-    }
-}
