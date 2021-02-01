1078e1f8274cf01a8249ec25881508c9ec95c169
jackhsu-git <contactjackhsu@gmail.com>
2021-02-01 15:13:33 +0800

    fix: fix loading visual bug

--- src/components/Card/ImageCard.vue
+++ src/components/Card/ImageCard.vue
@@ -1,6 +1,6 @@
 <template>
     <b-card id="image-card" @click="onCardClicked">
-        <b-skeleton-img v-show="isLoading"></b-skeleton-img>
+        <b-skeleton-img v-if="isLoading"></b-skeleton-img>
         <div v-show="!isLoading">
             <ProtectedImage :src="src" :alt="alt" imgClass="imgClass" @imageSuccess="handleImgSuccess()" />
             <span v-if="imageDetails.createBy">
