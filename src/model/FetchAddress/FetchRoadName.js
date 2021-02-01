c4169503af0034098b665169ff6d2875a784885e
jackhsu-git <contactjackhsu@gmail.com>
2021-02-01 09:56:42 +0800

    chore: remove test reports and update fetchRoadName method

--- src/model/FetchAddress/FetchRoadName.js
+++ src/model/FetchAddress/FetchRoadName.js
@@ -5,13 +5,12 @@ import * as fetch from 'node-fetch'
 export async function fetchRoadName(city, area) {
     const codedCity = encodeURI(city);
     const codedArea = encodeURI(area);
-    console.log(codedArea, codedCity)
     const result = await fetch(
         `https://cors-anywhere.herokuapp.com/https://www.post.gov.tw/post/streetNameData?city=${codedCity}&cityarea=${codedArea}`, {
             method: "POST"
         });
     const buffer = await result.arrayBuffer();
-    const text = iconv.decode(new Buffer(buffer), 'Big5');
+    const text = iconv.decode(Buffer.from(buffer), 'Big5');
     const converted = xmljs.xml2js(text, {
         compact: true,
         spaces: 4
--- test/FetchRoadName.test.js
+++ test/FetchRoadName.test.js
@@ -2,17 +2,17 @@ import {
     fetchRoadName
 } from '@/model/FetchAddress/fetchRoadName.js'
 import {
-    // assert,
-    expect
+    assert,
+    // expect
 } from "chai";
 
 describe("測試路名fetch function", () => {
     test('臺北市內湖區的街道應為array', async () => {
         try {
-            await fetchRoadName("臺北市", "內湖區");
-
+            const response = await fetchRoadName("臺北市", "內湖區")
+            assert(Array.isArray(response))
         } catch (e) {
-            expect(e).match("error");
+            console.log(e)
         }
     }, 1000);
 })
