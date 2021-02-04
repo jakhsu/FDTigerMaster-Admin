e164bfe9fa2520fc51e1305569f1001d36e0107d
jackhsu-git <contactjackhsu@gmail.com>
2021-02-04 16:54:48 +0800

    style🎨: fix formatting

--- src/components/Broadcast/BroadcastContent.vue
+++ src/components/Broadcast/BroadcastContent.vue
@@ -1,28 +1,42 @@
 <template>
-    <b-form>
-        <b-form-group label="通知標題">
-            <b-form-input v-model="title" type="text" placeholder="輸入通知標題"/>
-        </b-form-group>
-        <b-form-group label="通知文字">
-            <b-form-textarea
-                v-model="content"
-                placeholder="輸入通知文字"
-                rows="4"
-                max-rows="4"
-            ></b-form-textarea>
-        </b-form-group>
-        <b-form-group label="通知圖片(選用)">
-            <b-input-group inline>
-                <b-form-input v-model="imageUrl" type="text" placeholder="圖片連結"/>
-                <b-input-group-append>
-                    <b-button variant="outline-info">
-                        <font-awesome-icon icon="upload"/>
-                    </b-button>
-                </b-input-group-append>
-            </b-input-group>
-        </b-form-group>
-        <b-button variant="primary" :disabled="invaliedateForm" @click="onNextClick">下一步</b-button>
-    </b-form>
+  <b-form>
+    <b-form-group label="通知標題">
+      <b-form-input
+        v-model="title"
+        type="text"
+        placeholder="輸入通知標題"
+      />
+    </b-form-group>
+    <b-form-group label="通知文字">
+      <b-form-textarea
+        v-model="content"
+        placeholder="輸入通知文字"
+        rows="4"
+        max-rows="4"
+      />
+    </b-form-group>
+    <b-form-group label="通知圖片(選用)">
+      <b-input-group inline>
+        <b-form-input
+          v-model="imageUrl"
+          type="text"
+          placeholder="圖片連結"
+        />
+        <b-input-group-append>
+          <b-button variant="outline-info">
+            <font-awesome-icon icon="upload" />
+          </b-button>
+        </b-input-group-append>
+      </b-input-group>
+    </b-form-group>
+    <b-button
+      variant="primary"
+      :disabled="invaliedateForm"
+      @click="onNextClick"
+    >
+      下一步
+    </b-button>
+  </b-form>
 </template>
 
 <script>
@@ -35,14 +49,14 @@ export default {
             imageUrl: ''
         };
     },
-    methods:{
-        onNextClick(){
-            this.$emit('next', {title: this.title, content: this.content, imageUrl: this.imageUrl});
-        }
-    },
     computed:{
         invaliedateForm(){
             return this.title === '' || this.content  === '';
+        }
+    },
+    methods:{
+        onNextClick(){
+            this.$emit('next', {title: this.title, content: this.content, imageUrl: this.imageUrl});
         }
     }
 }
