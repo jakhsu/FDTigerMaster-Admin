55340ad68b78ffac86c4c380f916647b6ec56ab9
jackhsu-git <contactjackhsu@gmail.com>
2021-02-05 09:52:57 +0800

    feat✨: add order quote modal to show detailed fee
    need more work on layout and style of the modal info

--- src/views/Home/Order/OrderDetailComponent/OrderQuotes.vue
+++ src/views/Home/Order/OrderDetailComponent/OrderQuotes.vue
@@ -1,12 +1,23 @@
 <template>
     <div>
-        <TitledCard title="付款紀錄">
+        <SimpleModal id="quote-fee-modal" title="報價單細項">
+            <template #modal-body>
+                <b-card v-for="(fee, index) in parsedFee" :key="index">
+                    <template #header>
+                        {{fee.group}}
+                    </template>
+                    <ul>
+                        <li v-for="(entry, index) in fee.detail" :key="index">
+                            {{entry}}
+                        </li>
+                    </ul>
+                </b-card>
+            </template>
+        </SimpleModal>
+        <TitledCard title="報價單紀錄">
             <CustomTable :datas="data" :fields="field">
-                <template #cell(payFor)=data>
-                    {{paymentTypeMap[data.value]}}
-                </template>
-                <template #cell(paymentStatus)=data>
-                    {{paymentStatusMap[data.value]}}
+                <template #cell(fee)=data>
+                    <b-button variant="outline-success" @click="showFeeModal(data.value)">查看細項</b-button>
                 </template>
             </CustomTable>
         </TitledCard>
@@ -19,11 +30,13 @@
     import CustomTable from '@/components/Table/CustomTable.vue';
     import PaymentTypeMap from '@/model/Mapping/PaymentTypeMap.js';
     import PaymentStatusMap from '@/model/Mapping/PaymentStatusMap.js'
+    import SimpleModal from '@/components/Modal/SimpleModal.vue';
     export default {
         name: 'OrderQuotes',
         components: {
             TitledCard,
-            CustomTable
+            CustomTable,
+            SimpleModal
         },
         props: {
             order: {}
@@ -50,13 +63,19 @@
                         "label": "細項"
                     }
                 ],
-                queryRows: 0
+                queryRows: 0,
+                parsedFee: {}
             }
         },
-        created() {
-            this.fetchPaymentHistory();
+        async created() {
+            await this.fetchPaymentHistory();
         },
         methods: {
+            showFeeModal(fee) {
+                const parsed = JSON.parse(fee)
+                this.parsedFee = parsed;
+                this.$bvModal.show('quote-fee-modal')
+            },
             async fetchPaymentHistory() {
                 const res = await tigermaster.database.query("quote_price")
                     .where("quote_price.order_id", "=", this.order.id)
