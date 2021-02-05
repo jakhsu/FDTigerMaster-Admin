3f824f1eae1902ea9b782313aab01b28d422ba13
jackhsu-git <contactjackhsu@gmail.com>
2021-02-05 11:47:02 +0800

    fix🐛: fix case converter not converting multiple char(global regex)

--- src/model/CaseConverter/CaseConverter.js
+++ src/model/CaseConverter/CaseConverter.js
@@ -1,5 +1,5 @@
 export function camel2Snake(string) {
-    const regex = /[A-Z]/;
+    const regex = /[A-Z]/g;
     const modifiedString = string.replace(regex, function (match) {
         return "_" + match.toLowerCase();
     })
