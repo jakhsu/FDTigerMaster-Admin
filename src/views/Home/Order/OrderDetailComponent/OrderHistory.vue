b4514029038b145ec722cc5cfc2184647107c6ca
jackhsu-git <contactjackhsu@gmail.com>
2021-02-08 13:22:35 +0800

    Revert "WIP🚧: make custom table have less fields, need more work"
    This reverts commit 9ab83ffeed106914566db4da8c860441be42275a.

--- src/views/Home/Order/OrderDetailComponent/OrderHistory.vue
+++ src/views/Home/Order/OrderDetailComponent/OrderHistory.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <TitledCard title="訂單歷史">
-            <CustomTable :datas="orderHistory" :fields="fields" @rowClick="openOrderHistoryModal">
+            <CustomTable :datas="orderHistory">
             </CustomTable>
         </TitledCard>
     </div>
@@ -22,32 +22,7 @@
         },
         data() {
             return {
-                orderHistory: [],
-                fields: [{
-                        "label": "城市",
-                        "key": "addressCity"
-                    },
-                    {
-                        "label": "區",
-                        "key": "addressArea"
-                    },
-                    {
-                        "label": "街道",
-                        "key": "addressStreet"
-                    },
-                    {
-                        "label": "門牌樓層",
-                        "key": "addressDetail"
-                    },
-                    {
-                        "label": "訂單狀態",
-                        "key": "status"
-                    },
-                    {
-                        "label": "變更時間",
-                        "key": "orderUpdateDate"
-                    }
-                ]
+                orderHistory: Array
             }
         },
         methods: {
@@ -55,9 +30,6 @@
                 const res = await tigermaster.database.query("order_history")
                     .where("order_history.order_id", "=", this.order._orderId).get()
                 this.orderHistory = res.data
-            },
-            openOrderHistoryModal(data) {
-                console.log(data)
             }
         },
         created() {
