436d2467eb21eb4a8540b5fa5e362b44b6b2e76e
jackhsu-git <contactjackhsu@gmail.com>
2021-02-08 16:59:10 +0800

    WIP🚧: create chat room page

---
+++ src/views/Home/Order/OrderDetailComponent/ChatRoom.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <b-form>
+
+        </b-form>
+    </div>
+</template>
+
+<script>
+    import tigermaster from 'fdtigermaster-admin-sdk'
+    export default {
+        name: "ChatRoom",
+        data() {
+            return {
+                chatRoomUsers: []
+            }
+        },
+        methods: {
+            async createChatRoom() {
+                await tigermaster.chatroom.created({
+                    userIds: this.chatRoomUsers
+                })
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
