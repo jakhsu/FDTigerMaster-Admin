21ee791d2c1b72c712e06d35358c3facaaeab4b0
jackhsu-git <contactjackhsu@gmail.com>
2021-02-18 13:23:01 +0800

    feat✨: add simple image component for customization

---
+++ src/components/Image/SimpleImage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <img :src="src" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SimpleImage',
+        props: {
+            src: String
+        },
+        data() {
+            return {}
+        }
+    }
+</script>
