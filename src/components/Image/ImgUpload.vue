8c17bfc3c30f443a365a2f02b6b299dc9fe4676d
jackhsu-git <contactjackhsu@gmail.com>
2021-02-19 14:15:14 +0800

    cosmetic💄: add dotted box to emphasize upload img preview using some CSS hackery

--- src/components/Image/ImgUpload.vue
+++ src/components/Image/ImgUpload.vue
@@ -1,25 +1,35 @@
 <template>
     <div>
-        <b-col v-if="showsPreview" class="preview-area mb-2">
-            <img height="200" v-if="imagePath" :src="imagePath" alt="">
-        </b-col>
-        <b-col class="p-0">
-            <b-input-group>
-                <b-form-input v-model="imageFile.name" disabled></b-form-input>
-                <b-input-group-append>
-                    <input ref="file" type="file" @change="handleImage" class="custom-input" accept="image/*"
-                        style="display:none">
-                    <b-button @click="$refs.file.click()" variant="warning">
-                        選擇欲上傳圖片
-                    </b-button>
-                </b-input-group-append>
-            </b-input-group>
-        </b-col>
+        <TitledCard title="上傳圖片">
+            <div>
+                <b-col v-if="showsPreview" class="preview-area mb-2">
+                    <div class="preview">
+                        <img height="200" v-if="imagePath" :src="imagePath" alt="">
+                    </div>
+                </b-col>
+                <b-col class="p-0">
+                    <b-input-group>
+                        <b-form-input v-model="imageFile.name" disabled></b-form-input>
+                        <b-input-group-append>
+                            <input ref="file" type="file" @change="handleImage" class="custom-input" accept="image/*"
+                                style="display:none">
+                            <b-button @click="$refs.file.click()" variant="warning">
+                                選擇欲上傳圖片
+                            </b-button>
+                        </b-input-group-append>
+                    </b-input-group>
+                </b-col>
+            </div>
+        </TitledCard>
     </div>
 </template>
 
 <script>
+    import TitledCard from '@/components/Card/TitledCard.vue';
     export default {
+        components: {
+            TitledCard
+        },
         name: "ImgUpload",
         props: {
             showsPreview: {
@@ -57,5 +67,8 @@
 </script>
 
 <style scoped>
-
+    .preview {
+        background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='black' stroke-width='4' stroke-dasharray='2%2c 8' stroke-dashoffset='0' stroke-linecap='butt'/%3e%3c/svg%3e");
+        height: 210px;
+    }
 </style>
