34e376658d88c8f5ec37006a5f5ddadad4b028f8
jackhsu-git <contactjackhsu@gmail.com>
2021-02-19 10:00:40 +0800

    feat✨: finish basic broadcast history page

--- src/views/Home/Broadcast/BroadcastHistory.vue
+++ src/views/Home/Broadcast/BroadcastHistory.vue
@@ -1,19 +1,53 @@
 <template>
     <div>
-
+        <b-container fluid>
+            <TitledCard title="推播歷史">
+                <CustomTable :datas="msg" :isBusy="isLoading">
+                </CustomTable>
+            </TitledCard>
+        </b-container>
     </div>
 </template>
 
 <script>
+    import TitledCard from '@/components/Card/TitledCard.vue'
+    import CustomTable from '@/components/Table/CustomTable.vue'
+    import tigermaster from 'fdtigermaster-admin-sdk'
     export default {
+        components: {
+            TitledCard,
+            CustomTable
+        },
         name: 'BroadcastHistory',
         props: {},
         data() {
-            return
+            return {
+                msg: [],
+                queryRows: 0,
+                isLoading: false
+            }
         },
-        created() {},
+        created() {
+            this.fetchBroadcastHistory();
+        },
         methods: {
+            async fetchBroadcastHistory() {
+                this.isLoading = true;
+                const database = tigermaster.database;
+                const query = database.query("push_notify_history")
+                    .orderBy("push_notify_history.create_date", "DESC")
+                try {
+                    const res = await query.get()
+                    this.msg = res.data;
+                    this.queryRows = res.queryRows;
+                } catch (e) {
+                    this.msg = [];
+                    this.queryRows = 0;
+                } finally {
+                    this.isLoading = false;
+                }
 
+            }
         }
     }
 </script>
