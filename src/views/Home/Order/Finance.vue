5c13f815f545cb333c832becb7a0cf88f4c8cb25
jackhsu-git <contactjackhsu@gmail.com>
2021-02-25 17:28:18 +0800

    WIP🚧: create basic container for order finance page

--- src/views/Home/Order/Finance.vue
+++ src/views/Home/Order/Finance.vue
@@ -1,11 +1,29 @@
 <template>
     <div>
-
+        <b-container fluid>
+            <TitledCard title="匯款資訊">
+                <CustomTable :datas="orders">
+                </CustomTable>
+            </TitledCard>
+        </b-container>
     </div>
 </template>
 
 <script>
+    import TitledCard from '@/components/Card/TitledCard.vue'
+    import CustomTable from '@/components/Table/CustomTable.vue'
     export default {
-        name: 'Finance.vue'
+        components: {
+            TitledCard,
+            CustomTable
+        },
+        name: 'Finance.vue',
+        data() {
+            return {
+                orders: [{
+
+                }]
+            }
+        }
     }
 </script>
