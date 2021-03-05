cd9e80e031c1f9f12b7cfcb9e3bfe7982daeb8a3
jackhsu-git <contactjackhsu@gmail.com>
2021-03-05 17:18:53 +0800

    WIP🚧: service level setting page in progress
    now can edit L1 L2 items, but L3 doesn't now allow edit, only addition and deletion. addition and
    deletion functionality not implemented. data is fetched from DB. history listing and rollback not
    implemented. basic UI is finished, except for scrolling view window.

--- src/views/Home/Services/ServiceLevel.vue
+++ src/views/Home/Services/ServiceLevel.vue
@@ -7,60 +7,83 @@
                         <b-button block>
                             <font-awesome-icon icon="edit" />新增
                         </b-button>
-                        <CustomTable @rowClick="onRowClick($event, 'L1')" :datas="L1Items" :fields="fields">
-                            <template #footer v-if="this.currentL1.name">
-                                <b-card class="mb-2">
-                                    <div class="d-flex">
-                                        <b-button variant="outline-primary" class="ml-auto" v-if="!isEditL1">
-                                            <font-awesome-icon icon="edit" />修改</b-button>
-                                        <b-button variant="outline-danger" class="ml-2" v-if="!isEditL1">
-                                            <font-awesome-icon icon="trash-alt" />刪除</b-button>
-                                        <b-button variant="success" class="ml-auto" v-if="isEditL1">完成</b-button>
-                                    </div>
-                                    <div class="mt-2">
-                                        <b-form-group>
-                                            <b-form-input :disabled="!isEditL1" v-model="currentL1.name" />
-                                            <b-form-input :disabled="!isEditL1" class="mt-2" v-model="currentL1.desc" />
-                                            <b-form-input :disabled="!isEditL1" class="mt-2"
-                                                v-model="currentL1.imagePath" />
-                                        </b-form-group>
-                                    </div>
-                                </b-card>
-                            </template>
-                        </CustomTable>
+                        <b-list-group class="mt-2">
+                            <b-list-group-item @click="onListClick(item, 1)" v-for="(item, index) in L1Items"
+                                :key="index">
+                                {{item}}
+                            </b-list-group-item>
+                        </b-list-group>
+                        <b-card v-if="currentL1.name" class="mb-2">
+                            <div class="d-flex">
+                                <b-button variant="outline-primary" class="ml-auto" v-if="!isEditL1"
+                                    @click="allowL1Edit">
+                                    <font-awesome-icon icon="edit" />修改</b-button>
+                                <b-button variant="outline-danger" class="ml-2" v-if="!isEditL1">
+                                    <font-awesome-icon icon="trash-alt" />刪除</b-button>
+                                <b-button variant="success" class="ml-auto" v-if="isEditL1" @click="finishL1Edit">完成
+                                </b-button>
+                                <b-button class="ml-2" v-if="isEditL1" @click="cancelL1Edit">取消</b-button>
+                            </div>
+                            <div class="mt-2">
+                                <b-form-group>
+                                    <b-form-input :disabled="!isEditL1" v-model="currentL1.name" />
+                                    <b-form-input :disabled="!isEditL1" class="mt-2" v-model="currentL1.desc" />
+                                    <b-form-input :disabled="!isEditL1" class="mt-2" v-model="currentL1.imagePath" />
+                                </b-form-group>
+                            </div>
+                        </b-card>
                     </b-col>
                     <b-col sm=6 lg="4">
                         <b-button block>
                             <font-awesome-icon icon="edit" />新增
                         </b-button>
-                        <CustomTable @rowClick="onRowClick($event, 'L2')" :datas="L2Items" :fields="fields">
-                            <template #footer v-if="this.currentL2.name">
-                                <b-card class="mb-2">
-                                    <div class="d-flex">
-                                        <b-button variant="outline-primary" class="ml-auto" v-if="!isEditL2">
-                                            <font-awesome-icon icon="edit" />修改</b-button>
-                                        <b-button variant="outline-danger" class="ml-2" v-if="!isEditL2">
-                                            <font-awesome-icon icon="trash-alt" />刪除</b-button>
-                                        <b-button variant="success" class="ml-auto" v-if="isEditL2">完成</b-button>
-                                    </div>
-                                    <div class="mt-2">
-                                        <b-form-group>
-                                            <b-form-input :disabled="!isEditL2" v-model="currentL2.name" />
-                                            <b-form-input :disabled="!isEditL2" class="mt-2" v-model="currentL2.desc" />
-                                            <b-form-input :disabled="!isEditL2" class="mt-2"
-                                                v-model="currentL2.imagePath" />
-                                        </b-form-group>
-                                    </div>
-                                </b-card>
-                            </template>
-                        </CustomTable>
+                        <b-list-group class="mt-2">
+                            <b-list-group-item @click="onListClick(item, 2)" v-for="(item, index) in L2Items"
+                                :key="index">
+                                {{item}}
+                            </b-list-group-item>
+                        </b-list-group>
+                        <b-card v-if="currentL2.name" class="mb-2">
+                            <div class="d-flex">
+                                <b-button variant="outline-primary" class="ml-auto" v-if="!isEditL2"
+                                    @click="allowL2Edit">
+                                    <font-awesome-icon icon="edit" />修改</b-button>
+                                <b-button variant="outline-danger" class="ml-2" v-if="!isEditL2">
+                                    <font-awesome-icon icon="trash-alt" />刪除</b-button>
+                                <b-button variant="success" class="ml-auto" v-if="isEditL2" @click="finishL2Edit">完成
+                                </b-button>
+                                <b-button class="ml-2" v-if="isEditL2" @click="cancelL2Edit">取消</b-button>
+                            </div>
+                            <div class="mt-2">
+                                <b-form-group>
+                                    <b-form-input :disabled="!isEditL2" v-model="currentL2.name" />
+                                    <b-form-input :disabled="!isEditL2" class="mt-2" v-model="currentL2.desc" />
+                                    <b-form-input :disabled="!isEditL2" class="mt-2" v-model="currentL2.imagePath" />
+                                </b-form-group>
+                            </div>
+                        </b-card>
                     </b-col>
                     <b-col sm=6 lg="4">
                         <b-button block>
                             <font-awesome-icon icon="edit" />新增
                         </b-button>
-                        <CustomTable :datas="L3Items" :fields="fields">
-                        </CustomTable>
+                        <b-list-group class="mt-2">
+                            <b-list-group-item @click="onListClick(item, 3)" v-for="(item, index) in L3Items"
+                                :key="index">
+                                {{item}}
+                            </b-list-group-item>
+                        </b-list-group>
+                        <b-card v-if="currentL3" class="mb-2">
+                            <div class="d-flex">
+                                <b-button variant="outline-danger" class="ml-2">
+                                    <font-awesome-icon icon="trash-alt" />刪除</b-button>
+                            </div>
+                            <div class="mt-2">
+                                <b-form-group>
+                                    <b-form-input disabled v-model="currentL3" />
+                                </b-form-group>
+                            </div>
+                        </b-card>
                     </b-col>
                 </b-row>
             </TitledCard>
@@ -70,161 +93,75 @@
 
 <script>
     import TitledCard from '@/components/Card/TitledCard.vue'
-    import CustomTable from '@/components/Table/CustomTable.vue'
-    import example from './serviceLevelExample.json'
+    import tigermaster from 'fdtigermaster-admin-sdk'
+
     export default {
         name: 'ServiceLevel',
         components: {
             TitledCard,
-            CustomTable
         },
         data() {
             return {
-                fields: [
-                    "name"
-                ],
                 currentL1: {},
                 currentL2: {},
+                currentL3: "",
                 isEditL1: false,
                 isEditL2: false,
-                example,
+                serviceLevelData: {},
                 L1Items: [],
                 L2Items: [],
-                L3Items: []
+                L3Items: [],
+                jsonFile: {}
             }
         },
-        created() {
-            this.parseAllClassItems();
+        async created() {
+            await this.fetchServiceLevel()
+            this.parseAllClassItems()
         },
         methods: {
-            onRowClick(row, type) {
-                if (type == 'L1') {
-                    this.currentL1 = row
-                } else if (type == 'L2') {
-                    this.currentL2 = row
-                }
+            allowL1Edit() {
+                this.isEditL1 = true;
             },
-            parseL1Items(json) {
-                const data = []
-                for (let key in json) {
-                    const {
-                        name: name,
-                        desc: desc,
-                        imagePath: imagePath
-                    } = json[key]
-                    data.push({
-                        name,
-                        desc,
-                        imagePath
-                    })
-                }
-                return data
+            finishL1Edit() {
+                this.isEditL1 = false;
             },
-            parseL2Items(json) {
-                const data = []
-                for (let key in json) {
-                    const {
-                        L2: L2
-                    } = json[key]
-                    for (let key in L2) {
-                        const {
-                            name: name,
-                            desc: desc,
-                            imagePath: imagePath
-                        } = L2[key]
-                        data.push({
-                            name,
-                            desc,
-                            imagePath
-                        })
-                    }
-                }
-                return data
+            cancelL1Edit() {
+                this.isEditL1 = false;
             },
-            parseL3Items(json) {
-                const data = []
-                for (let key in json) {
-                    const {
-                        L2: L2
-                    } = json[key]
-                    for (let key in L2) {
-                        const {
-                            L3: L3
-                        } = L2[key]
-                        for (let key in L3) {
-                            data.push({
-                                name: L3[key]
-                            })
-                        }
-                    }
+            allowL2Edit() {
+                this.isEditL2 = true;
+            },
+            finishL2Edit() {
+                this.isEditL2 = false;
+            },
+            cancelL2Edit() {
+                this.isEditL2 = false;
+            },
+            onListClick(item, level) {
+                this.parseAllClassItems()
+                if (level === 1) {
+                    this.currentL1 = this.serviceLevelData[item]
+                } else if (level === 2) {
+                    this.currentL2 = this.currentL1.L2[item]
+                } else if (level === 3) {
+                    const index = this.currentL2.L3.indexOf(item)
+                    this.currentL3 = this.serviceLevelData[this.currentL1.name]
+                        .L2[this.currentL2.name]
+                        .L3[index]
                 }
-                return data
             },
             parseAllClassItems() {
-                this.L1Items = this.parseL1Items(this.example)
-                this.L2Items = this.parseL2Items(this.example)
-                this.L3Items = this.parseL3Items(this.example)
+                this.L1Items = Object.keys(this.serviceLevelData)
+                const selectedL1 = this.currentL1.name || this.L1Items[0]
+                this.L2Items = Object.keys(this.serviceLevelData[selectedL1].L2)
+                const selecctedL2 = this.currentL2.name || this.L2Items[0]
+                this.L3Items = this.serviceLevelData[selectedL1].L2[selecctedL2].L3
+            },
+            async fetchServiceLevel() {
+                const serviceLevel = tigermaster.services.Level;
+                this.serviceLevelData = await serviceLevel.get()
             }
         },
-        // computed: {
-        //     L1Items: function () {
-        //         const example = this.example;
-        //         const data = []
-        //         for (let key in example) {
-        //             const {
-        //                 name: name,
-        //                 desc: desc,
-        //                 imagePath: imagePath
-        //             } = example[key]
-        //             data.push({
-        //                 name,
-        //                 desc,
-        //                 imagePath
-        //             })
-        //         }
-        //         return data
-        //     },
-        //     L2Items: function () {
-        //         const example = this.example;
-        //         const data = []
-        //         for (let key in example) {
-        //             const {
-        //                 L2: L2
-        //             } = example[key]
-        //             for (let key in L2) {
-        //                 const {
-        //                     name: name,
-        //                     desc: desc,
-        //                     imagePath: imagePath
-        //                 } = L2[key]
-        //                 data.push({
-        //                     name,
-        //                     desc,
-        //                     imagePath
-        //                 })
-        //             }
-        //         }
-        //         return data
-        //     },
-        //     L3Items: function () {
-        //         const example = this.example;
-        //         const data = []
-        //         for (let key in example) {
-        //             const {
-        //                 L2: L2
-        //             } = example[key]
-        //             for (let key in L2) {
-        //                 const {
-        //                     L3: L3
-        //                 } = L2[key]
-        //                 for (let key in L3) {
-        //                     data.push(L3[key])
-        //                 }
-        //             }
-        //         }
-        //         return data
-        //     },
-        // }
     }
 </script>
 
