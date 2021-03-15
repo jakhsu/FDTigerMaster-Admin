744df6f8023e9c6f0ea3a411d142656d1562638b
jackhsu-git <contactjackhsu@gmail.com>
2021-03-15 11:40:38 +0800

    WIP🚧: finance page mock data

--- src/views/Home/Order/Finance.vue
+++ src/views/Home/Order/Finance.vue
@@ -2,7 +2,10 @@
     <div>
         <b-container fluid>
             <TitledCard title="匯款資訊">
-                <CustomTable :datas="orders">
+                <CustomTable :datas="example" :fields="fields">
+                    <template #cell(action)>
+                        <b-button variant="outline-success">繳費</b-button>
+                    </template>
                 </CustomTable>
             </TitledCard>
         </b-container>
@@ -12,17 +15,38 @@
 <script>
     import TitledCard from '@/components/Card/TitledCard.vue'
     import CustomTable from '@/components/Table/CustomTable.vue'
+    import example from "./finance_example.json"
     export default {
         components: {
             TitledCard,
             CustomTable
         },
-        name: 'Finance.vue',
+        name: 'Finance',
         data() {
             return {
-                orders: [{
+                example,
+                fields: [{
+                        label: "id",
+                        key: "id"
 
-                }]
+                    },
+                    {
+                        label: "付款狀態",
+                        key: "payment_status"
+
+                    }, {
+                        label: "金額",
+                        key: "amount"
+
+                    }, {
+                        label: "期限",
+                        key: "deadline"
+
+                    }, {
+                        label: "動作",
+                        key: "action"
+                    }
+                ]
             }
         }
     }
