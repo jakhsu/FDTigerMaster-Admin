454c0c7d25396991e27080b6cc084593bbbaa778
jackhsu-git <contactjackhsu@gmail.com>
2021-03-22 09:48:19 +0800

    remove🔥: keep app.vue clean and extract chatroom polling logic to chatroom list component

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,17 @@
   <div id="app">
     <Loading v-if="isLoading" />
     <router-view v-else />
-    <Chatroom v-if="$store.state.chatroom.isShow" class="chatroom" />
   </div>
 </template>
 
 <script>
   import Loading from '@/components/Loading'
-  import Chatroom from '@/components/Chatroom/Chatroom.vue'
   import tigermaster from 'fdtigermaster-admin-sdk'
-  import store from './store'
 
   export default {
     name: "App",
     components: {
-      Loading,
-      Chatroom
-
+      Loading
     },
     data() {
       return {
@@ -29,34 +24,7 @@
       await tigermaster.initializeApp({
         stage: process.env.VUE_APP_STAGE
       });
-      this.fetctAdminChatroomId()
       this.isLoading = false;
-    },
-    methods: {
-      async fetctAdminChatroomId() {
-        let query = tigermaster.database.query("chatroom").where("chatroom.user_ids", "LIKE",
-          `%${2020123000004}%`)
-        try {
-          let res = await query.get()
-          const ids = res.data.map(e => e.id)
-          store.commit('setChatroomIds', ids)
-        } catch (e) {
-          console.log(e)
-        }
-      },
-      async pollChatroomMsg() {
-        // TODO: poll approciate msg using chatroom id, maybe use 'setInterval' to achieve polling?
-        const ids = store.state.chatroom.ids;
-        console.log(ids)
-        let query = tigermaster.database.query("chatroom").where("chatroom.user_ids", "LIKE",
-          `%${2020123000004}%`)
-        try {
-          let res = await query.get()
-          console.log(res.data.map(e => e.id))
-        } catch (e) {
-          console.log(e)
-        }
-      }
     }
   }
 </script>
